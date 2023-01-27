701f0f62124a677d7ee6546e55b4f0e5cddec4d1
stefansnaerh <stefansnaerh@gmail.com>
2023-01-27 13:53:19 +0000

    made comments explaining CORS

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,30 @@
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config({path: '.env'});
+const cors = require('cors')
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5001
+
 
 connectDB()
 
 const app = express()
 
+
+app.use(cors())
+/*
+app.use(cors({
+    origin: 'www.carsales.com',
+    methods: ["GET", "POST", "DELETE", "PUT"]
+}))
+*/
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
+
 app.use('/api/cars', require('./routes/carRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
-
-
 
 app.listen(port, () => console.log((`Server started on port ${port}`)))
