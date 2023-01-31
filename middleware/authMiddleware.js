791f65023f52874bc55446fb9de341cba80f0f9a
solveighelga <111748545+solveighelga@users.noreply.github.com>
2023-01-31 11:48:18 +0000

    authMiddleware update

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
-const User = require('../../models/userModel')
+const User = require('../models/userModel')
 
 
 const protect = asyncHandler(async (req, res ,next) => {
