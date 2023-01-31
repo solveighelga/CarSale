e46d6e9a4ed017437a19efc93d7793c1b58e4b57
solveighelga <111748545+solveighelga@users.noreply.github.com>
2023-01-31 11:45:17 +0000

    removed backend folder

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
-const User = require('../models/userModel')
+const User = require('../../models/userModel')
 
 // desc : Register user
 // route : POST /api/users
